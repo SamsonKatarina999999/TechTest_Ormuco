7906392fc6c7fd9054bbfd63ab70aeef48551bce
MaxiAgrippa <60492296+MaxiAgrippa@users.noreply.github.com>
2021-09-14 23:47:38 -0400

    Add project files.

---
+++ TechTest_Ormuco/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechTest_Ormuco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
